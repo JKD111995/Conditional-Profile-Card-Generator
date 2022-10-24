8b53a71c64dbcccab31c1674376441f5e9ec5520
JKD111995 <jake.duque@gmail.com>
2022-10-24 23:44:18 +0000

    Done

--- src/js/app.js
+++ src/js/app.js
@@ -45,11 +45,11 @@ function render(variables = {}) {
           <h1>${variables.name} ${variables.lastname}</h1>
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
-          <ul class="position-right">
-            <li><a href=">${variables.twitter}"<i class="fab fa-twitter"></i></a></li>
-            <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://www.twitter.com/${variables.twitter}"<i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
